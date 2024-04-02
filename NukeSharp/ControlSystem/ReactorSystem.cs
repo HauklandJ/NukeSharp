cdd5895f84389c748336c654dab23881de3d8a80
HauklandJ <johannes.haukland@digdir.no>
2024-04-02 14:58:40 +0200

    refactor: rm magic numbers

--- NukeSharp/ControlSystem/ReactorSystem.cs
+++ NukeSharp/ControlSystem/ReactorSystem.cs
@@ -8,6 +8,9 @@ public class ReactorSystem : IReactorSystem
     private readonly IPressureSensor _pressureSensor;
     private readonly ILogger<ReactorSystem> _logger;
 
+    private readonly float MAX_PRESSURE = 0.71f;
+    private readonly float MIN_PRESSURE = 0.57f;
+
     public ReactorSystem(IValveControl valveControl, IPressureSensor pressureSensor, ILogger<ReactorSystem> logger)
     {
         _valveControl = valveControl;
@@ -20,11 +23,11 @@ public class ReactorSystem : IReactorSystem
     private void HandlePressureChange(float newPressure)
     {
         _logger.LogInformation($"SYSTEM current value: {newPressure}");
-        if (newPressure > 0.71f && !_valveControl.IsOpen())
+        if (newPressure > MAX_PRESSURE && !_valveControl.IsOpen())
         {
             _valveControl.Open();
         }
-        else if (newPressure < 0.57f && _valveControl.IsOpen())
+        else if (newPressure < MIN_PRESSURE && _valveControl.IsOpen())
         {
             _valveControl.Close();
         }
