b6b7d4d47711b24e1e1770f68ccd09f047193734
HauklandJ <hauklandjohannes@gmail.com>
2026-04-23 09:56:19 +0200

    refactor: remove manual config builder setup

--- NukeSharp/Program.cs
+++ NukeSharp/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.IO;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.FileProviders;
 using NukeSharp.Controllers;
 using NukeSharp.ControlSystem;
@@ -10,51 +10,38 @@ using NukeSharp.Services;
 using NukeSharp.Simulator;
 using NukeSharp.Workers;
 
-IConfigurationBuilder configBuilder = new ConfigurationBuilder()
-    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-    .AddJsonFile("appsettings.json");
-
-IConfigurationRoot configuration = configBuilder.Build();
-
-string pressureDeviationType = configuration["PressureDeviationType"] ?? "standard";
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+string pressureDeviationType = builder.Configuration["PressureDeviationType"] ?? "standard";
+
 builder.Services.AddSingleton<IValveControl, ValveControl>();
 
 if (pressureDeviationType == "standard")
 {
     builder.Services.AddSingleton<IPressureSensor, PressureSensor>();
-}
+} 
 else if (pressureDeviationType == "randomized")
 {
     builder.Services.AddSingleton<IPressureSensor, RandomizedPressureSensor>();
 }
-else
+else 
 {
-    throw new InvalidOperationException(
-        $"Invalid value for environment variable 'PressureDeviationType'. Expected 'Standard' or 'Randomized', but got '{pressureDeviationType}'."
-    );
+    throw new InvalidOperationException($"Invalid value for environment variable 'PressureDeviationType'. Expected 'Standard' or 'Randomized', but got '{pressureDeviationType}'.");
 }
+
 
 builder.Services.AddSingleton<IReactor, Reactor>();
 builder.Services.AddSingleton<ReactorSystem>();
-builder.Services.AddSingleton<EndPoints>();
 builder.Services.AddHostedService<ReactorWorker>();
 
 WebApplication app = builder.Build();
 app.Services.GetRequiredService<ReactorSystem>();
 
-EndPoints endpoints = app.Services.GetRequiredService<EndPoints>();
+IPressureSensor pressureSensor = app.Services.GetRequiredService<IPressureSensor>();
+ILogger<EndPoints> logger = app.Services.GetRequiredService<ILogger<EndPoints>>();
+EndPoints endpoints = new(pressureSensor, logger);
 
-app.UseStaticFiles(
-    new StaticFileOptions
-    {
-        FileProvider = new PhysicalFileProvider(
-            Path.Combine(Directory.GetCurrentDirectory(), "static")
-        ),
-        RequestPath = "/static",
-    }
-);
+app.UseStaticFiles(new StaticFileOptions { FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "static")), RequestPath = "/static" });
 
 app.MapGet("/", endpoints.GetIndex);
 app.MapGet("/pressure", endpoints.GetCurrentPressure);
